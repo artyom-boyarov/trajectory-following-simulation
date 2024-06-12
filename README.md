d7e9ef277418642387cb3b7f914062bc50f6e836
Artyom E. Boyarov <artyom.boyarov@gmail.com>
2024-06-12 14:10:30 -0400

    Cleaned up repo

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # Simulation of various trajectory following algorithms
-This repository contains code I used in a research project to compare multiple lateral control algorithms.
+This repository contains code for a simulation that shows a bus traveling along a trajectory in an urban environment. The bus uses a specified control algorithm to follow the trajectory. Implemented control algorithms include Stanley control, Pure Pursuit control, and some variations.
+<br>
+I used this simulation in my first research project where I compared the effectiveness of multiple lateral control algorithms in driving an autonomous bus around a track in an urban environment. The article for this research is available at <a>https://emerginginvestigators.org/articles/22-104</a>. See <a href="Abstract.md">Abstract.md</a>.
+<br>
+Feel free to use this code for your own research or experimenting, or to see implementations of the Stanley and Pure Pursuit control algorithms.
+
 ## This repo
 ```results/``` Contains excel spreadsheets of results.<br>
 ```tracks/``` Contains waypoint files to be used.<br>
-```main.py``` This file runs the simulation once for one lateral control algorithm.<br>
+```course_images/``` Contains diagrams of the courses used in the simulation. <br>
 ```simulation.py``` Contains the simulation class used with functions for steering.<br>
-```master.py``` Contains the code which runs all control algorithms and collects results in csv format.<br>
-All files can be run with ```--help``` to print out options
+```main.py``` Contains the code which runs all control algorithms, or just one, and collects results in csv format.<br>
+```lateral_controllers.py``` Contains implementations of the control algorithms used.<br>
+```all.sh``` A script to run the simulation across all courses and all algorithms and all speeds.<br>
+
 ## Running
-Note: Only python 3.6 has been shown to work with this code.<br>
+Create virtual environment: <br>
+```python -m venv .venv```<br>
+```source .venv/bin/activate```<br>
 To run install requirements first:<br>
 ```pip install -r requirements.txt```<br>
 Then run each file, e.g.<br>
-```python3.6 master.py```
+```python main.py```
--- main.py
+++ main.py
@@ -1,43 +1,150 @@
-import argparse
 from simulation import Simulation
-from lateral_controllers import *
+import matplotlib.pyplot as plt
+import argparse
 import sys, os
 
-if __name__ == "__main__":
+CONTROLLERS = ["Pure Pursuit", "Stanley", "Stanley with lookahead", "Hybrid Stanley and Pure Pursuit"]
 
-    controllers = ["Pure Pursuit", "Stanley", "Stanley with lookahead", "Hybrid Stanley and Pure Pursuit"]
+def write_data(array, times, filename, controllers):
+
+    file = open(filename, "wt+")
+    print(*times)
+    print(*(["Time"]+controllers), sep=',', file=file)
+    for i in range(len(times[0])-1):
+        print(*[times[0][i], array[0][i],array[1][i],array[2][i],array[3][i]], sep=',', file=file)
+    file.close()
+
+
+
+waypoint_file = "tracks/test_track_waypoints.txt"
+def main(waypoint_file: str = waypoint_file, speed: int = 50, time: int=20, show: bool=False, controller: str=None):
+    assert show == False
+
+    if controller is not None:
+        test_controllers = [controller]
+    else:
+        test_controllers = CONTROLLERS
+    ce_err = []
+    rce_err = []
+    yaw = []
+    yaw_change = []
+    total_ce = []
+    total_rce = []
+    times = []
+    for control in test_controllers:
+        print(control)
+        sim = Simulation(control, control, speed, waypoint_file, time, False)
+        results = sim.run()
+        ce_err.append(results[0])
+        rce_err.append(results[1])
+        yaw.append(results[2])
+        total_ce.append(results[3])
+        total_rce.append(results[4])
+        times.append(results[5])
+        yaw_change.append(results[6])
+    print("times:", times)
+    print("yaw_change:", yaw_change)
+    print("total_ce:", total_ce)
+    if show:
+        ce_fig = plt.figure(figsize=(4,4))
+        ce_ax = ce_fig.add_subplot(111)
+        for i in range(len(test_controllers)):
+            ce_ax.plot(times[i], ce_err[i], label=test_controllers[i].replace(" ", "\n"))
+
+        ce_ax.set_title("Crosstrack error")
+        ce_ax.set_xlabel("Time $s$")
+        ce_ax.set_ylabel("Crosstrack error $m$")
+        plt.legend()
+
+        total_ce_fig = plt.figure(figsize=(4,4))
+        total_ce_ax = total_ce_fig.add_subplot(111)
+        total_ce_ax.bar([controller.replace(" ", "\n") for controller in test_controllers], total_ce)
+        total_ce_ax.set_title("Total Front Crosstrack Error")
+        total_ce_ax.set_xlabel("Controller")
+        total_ce_ax.set_ylabel("Crosstrack error $m$")
+
+
+        rce_fig = plt.figure(figsize=(4,4))
+        rce_ax = rce_fig.add_subplot(111)
+        for i in range(len(test_controllers)):
+            rce_ax.plot(times[i], rce_err[i], label=test_controllers[i].replace(" ", "\n"))
+
+        rce_ax.set_title("Rear Crosstrack error")
+        rce_ax.set_xlabel("Time $s$")
+        rce_ax.set_ylabel("Rear Crosstrack error $m$")
+        plt.legend()
+        total_rce_fig = plt.figure(figsize=(4,4))
+        total_rce_ax = total_rce_fig.add_subplot(111)
+        total_rce_ax.bar([controller.replace(" ", "\n") for controller in test_controllers], total_rce)
+        print("Total rear error:", total_rce)
+        print("Total error:", total_ce)
+        total_rce_ax.set_title("Total Rear Crosstrack Error")
+        total_rce_ax.set_xlabel("Controller")
+        total_rce_ax.set_ylabel("Rear crosstrack error $m$")
+        plt.xticks(rotation=45, ha="right")
+
+
+
+        yaw_fig = plt.figure(figsize=(4,4))
+        yaw_ax = yaw_fig.add_subplot(111)
+        for i in range(len(test_controllers)):
+            yaw_ax.plot(times[i], yaw[i], label=test_controllers[i].replace(" ", "\n"))
+
+        yaw_ax.set_title("Steering angle")
+        yaw_ax.set_xlabel("Time $s$")
+        yaw_ax.set_ylabel("Steering angle $rad$")
+        plt.legend()
+
+        yawc_fig = plt.figure(figsize=(4,4))
+        yawc_ax = yawc_fig.add_subplot(111)
+        for i in range(len(test_controllers)):
+            yawc_ax.plot(times[i], yaw_change[i], label=test_controllers[i].replace(" ", "\n"))
+
+        yawc_ax.set_title("Steering angle change at 50 km/h")
+        yawc_ax.set_xlabel("Time $s$")
+        yawc_ax.set_ylabel("Steering angle change $rad$")
+
+        plt.legend()
+        plt.show()
+
+    prefix = f"results/{waypoint_file.split('/')[-1]}_{speed}_"
+    write_data(rce_err, times, f"{prefix}rear_ce.csv", test_controllers)
+    write_data(yaw_change, times, f"{prefix}yaw_change.csv", test_controllers)
+    write_data(ce_err, times, f"{prefix}ce_err.csv", test_controllers)
+    write_data(yaw, times, f"{prefix}yaw.csv", test_controllers)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Master script for lateral control")
+    parser.add_argument('-w', '--waypoint-file',metavar='WAYPOINT_FILE',dest='waypoint_file',required=False,default=waypoint_file)
+    parser.add_argument('-s', '--speed', metavar='SPEED', dest='speed', required=True)
+    parser.add_argument('-t', '--time', metavar='TIME', dest='time', required=True)
+    parser.add_argument('-o', '--show',  dest='show', default=False, action='store_true')
     argparser = argparse.ArgumentParser(description="Lateral control simulation")
     argparser.add_argument(
         '-l', '--lateral-controller',
         metavar='LATERAL_CONTROLLER',
         dest='lateral_controller',
-        choices=controllers,
-        help=f'Lateral controller to choose: {controllers}',
-        default=controllers[0]
+        choices=CONTROLLERS,
+        help=f'Lateral controller to choose: {CONTROLLERS}',
+        default=CONTROLLERS[0],
+        required=False
     )
-    argparser.add_argument(
-        '-w', '--waypoint-file',
-        metavar='WAYPOINT_FILE',
-        dest='waypoint_file',
-        help=f'Waypoint file to choose',
-        default='waypoints.txt'
-    )
-    args = argparser.parse_args()
-    controller: str
     waypoint_file: str
+    speed: str
+    time: str
+    show = False
     if len(sys.argv) < 2:
-        print("Please choose control algorithm")
-        print("Options:", *[f"{i:}: {controllers[i]}" for i in range(len(controllers))])
-        controller = controllers[int(input())]
+        
         print("Enter waypoint file name:")
         print(f"Suggested waypoint files:", *['tracks/' + x for x in os.listdir('./tracks/')])
         waypoint_file = input()
+        time = input("Enter time: ")
+        speed = input("Enter speed: ")
     else:
-        controller = args.lateral_controller
+        args = parser.parse_args()
         waypoint_file = args.waypoint_file
-
-    sim = Simulation("Lateral control",controller,50,  waypoint_file, 25)
-    try:
-        sim.run()
-    except KeyboardInterrupt:
-        print("Cancelled by user")
+        speed = args.speed
+        time = args.time
+        show = args.show
+    main(waypoint_file, int(speed), int(time), show, )
